e7569d3f681d4ff40576c1af6e5a156cc955958a
hung123987 <ngochungtran1412@gmail.com>
2019-10-30 15:07:43 +0700

    chapter12:password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = Settings.constant
 
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save :downcase_email
   before_create :create_activation_digest
@@ -9,9 +9,10 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: Settings.num50}
   validates :email, presence: true, length: {maximum: Settings.num255},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: true
-  validates :password, presence: true, length: {minimum: Settings.num6}
   validates :password, presence: true, length: {minimum: Settings.num6},
     allow_nil: true
+  validates :password_confirmation, presence: true,
+    length: {minimum: Settings.num6}, allow_nil: true
 
   has_secure_password
 
@@ -24,10 +25,10 @@ class User < ApplicationRecord
              end
       BCrypt::Password.create(string, cost: cost)
     end
-  end
 
-  def self.new_token
-    SecureRandom.urlsafe_base64
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 
   def remember
@@ -37,7 +38,7 @@ class User < ApplicationRecord
 
   def authenticated? attribute, token
     digest = send "#{attribute}_digest"
-    return false if digest.nil?
+    return false unless digest
 
     BCrypt::Password.new(digest).is_password?(token)
   end
@@ -52,6 +53,19 @@ class User < ApplicationRecord
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.n2.hours.ago
   end
 
   private
